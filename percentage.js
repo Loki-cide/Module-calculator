108f6d1876a89ff199b828a4586bd6667e3e3def
Rohit Shrestha <stharohit1245@gmail.com>
2026-06-28 22:30:35 +0545

    Add files via upload

---
+++ percentage.js
@@ -0,0 +1,10 @@
+const {ask} = require("./input");
+
+async function percentage() {
+    const a = Number(await ask("Enter a: "));
+    const b = Number(await ask("Enter b: "));
+
+    console.log(`Result = $(a/b *100)%`);
+}
+
+module.exports = percentage;
