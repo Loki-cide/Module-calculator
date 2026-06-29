ce2d05b16c3c92ea248fe5c34415a0061a83fa57
Loki-cide <stharohit1245@gmail.com>
2026-06-29 21:41:56 +0545

    percentage

--- percentage.js
+++ percentage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const {ask} = require("./input");
 
 async function percentage() {
@@ -8,15 +7,5 @@ async function percentage() {
     console.log(`Result = $(a/b *100)%`);
 }
 
-=======
-const {ask} = require("./input");
 
-async function percentage() {
-    const a = Number(await ask("Enter a: "));
-    const b = Number(await ask("Enter b: "));
-
-    console.log(`Result = $(a/b *100)%`);
-}
-
->>>>>>> 108f6d1876a89ff199b828a4586bd6667e3e3def
 module.exports = percentage;
